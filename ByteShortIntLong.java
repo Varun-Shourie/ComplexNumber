8aa9754747a51c500f57829195117f26f479812e
Varun Shourie <53848748+Varun-Shourie@users.noreply.github.com>
2021-01-15 12:27:12 -0700

    Add files via upload

---
+++ ByteShortIntLong.java
@@ -0,0 +1,47 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE; //Wrapper class - performs operations on primitive types.
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("long minimum value = " + myMinLongValue);
+        System.out.println("long maximum value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_284L;
+        //Must put an L at the end since Java treats number as an integer.
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2); //myMinByteVariable treated as int
+        // Do casting to treat the expression as byte instead of int.
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+    }
+}
